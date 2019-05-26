a5f3a7bc1157447f7a9a615eb20cbe26437f479c
poonasq257 <poonasp257@naver.com>
2019-05-26 16:31:48 +0900

    Initial commit

---
+++ Engine/GraphicsClass.h
@@ -0,0 +1,59 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: graphicsclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _GRAPHICSCLASS_H_
+#define _GRAPHICSCLASS_H_
+
+
+///////////////////////
+// MY CLASS INCLUDES //
+///////////////////////
+#include "d3dclass.h"
+#include "cameraclass.h"
+#include "modelclass.h"
+#include "lightshaderclass.h"
+#include "lightclass.h"
+#include "textureshaderclass.h"
+#include "bitmapclass.h"
+#include "textclass.h"
+#include "InputClass.h"
+
+#include <vector>
+
+/////////////
+// GLOBALS //
+/////////////
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = false;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: GraphicsClass
+////////////////////////////////////////////////////////////////////////////////
+class GraphicsClass
+{
+public:
+	GraphicsClass();
+	GraphicsClass(const GraphicsClass&);
+	~GraphicsClass();
+
+	bool Initialize(int, int, HWND, InputClass*);
+	void Shutdown();
+	bool Frame(int, float, int, int, int);
+
+private:
+	bool Render();
+
+private:
+	D3DClass* m_D3D;
+	InputClass* m_Input;
+	CameraClass* m_Camera;
+	std::vector<ModelClass*> m_Models;
+	LightShaderClass* m_LightShader;
+	LightClass* m_Light;
+	TextureShaderClass* m_TextureShader;
+	BitmapClass* m_Bitmap;
+	TextClass* m_Text;
+};
+#endif
